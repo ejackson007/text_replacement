202def57873aec7ddbc33697ab8804e5253395f1
Evan Jackson <evanojackson@gmail.com>
2021-02-21 22:19:31 -0600

    syntax error on sql call

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sqlite3 as sl
 import os.path
 from pynput.keyboard import Key, Listener
 import pyautogui
+from easygui import multenterbox
+
+reserved = ["new", "delete", "modify", "view_all"]
 
 if not os.path.exists("replacements.db"):
     con = sl.connect("replacements.db")
@@ -43,17 +46,38 @@ def on_press(key):
             candidate = ""
             candidate = candidate.join(typed)
             if candidate != "":
-                con = sl.connect("replacements.db")
-                with con:
-                    replace = con.execute(
-                        f"SELECT replace FROM REPLACEMENTS WHERE short='{candidate}'"
-                    )
-                    pyautogui.press('backspace', presses=len(candidate) +
-                                    3)  # // + word + " "
-                    #gets returned as a tuple, so we have to extract the first value
-                    pyautogui.typewrite([row[0] for row in replace][0])
-                    listening = False
+                if candidate not in reserved:
+                    con = sl.connect("replacements.db")
+                    with con:
+                        replace = con.execute(
+                            f"SELECT replace FROM REPLACEMENTS WHERE short='{candidate}'"
+                        )
+                        pyautogui.press('backspace',
+                                        presses=len(candidate) +
+                                        2)  # # + word + " "
+                        #gets returned as a tuple, so we have to extract the first value
+                        pyautogui.typewrite([row[0] for row in replace][0])
+                        listening = False
+                else:
+                    if candidate == "new": create_new()
 
+
+def create_new():
+    msg = "Create New Shortcut"
+    title = "Create New Shortcut"
+    fieldNames = ["Shortcut", "Expansion"]
+    fieldValues = multenterbox(msg, title, fieldNames)
+    con = sl.connect("replacements.db")
+    with con:
+        con.execute(f"""
+            INSERT INTO REPLACEMENTS(short, replace)
+            SELECT {fieldValues[0]}, {fieldValues[1]}
+            WHERE NOT EXISTS(SELECT 1 FROM REPLACEMENTS WHERE short = {fieldValues[0]});
+        """)
+    pyautogui.press('backspace', presses=5)
+
+
+create_new()
 
 with Listener(on_press=on_press) as listener:
     listener.join()
